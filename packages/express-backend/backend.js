2759f0b7599ff4b53ed47dc0ee60b48ca6d18dc9
Almas <apervmm@gmail.com>
2024-11-04 14:57:56 -0800

    nav

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -4,11 +4,63 @@ import express from "express";
 const app = express();
 const port = 8000;
 
+
+const api = "07a042f2cdf746179eb011e0cafcfb4c"
+
+
+
+
+
+const recipes = {
+  recipes: [
+    {
+      id: "1",
+      meal: "Pasta Carbonara",
+      author: "Almas",
+    },
+    {
+      id: "2",
+      meal: "Mac&Cheese",
+      aruthor: "Max"
+    },
+    {
+      id: "3",
+      meal: "Pepperoni Pizza",
+      author: "Andy"
+    },
+    {
+      id: "4",
+      meal: "Ckicken Noodle Soup",
+      author: "Hudson"
+    },
+    {
+      id: "5",
+      meal: "Tartar",
+      author: "Kyle"
+    },
+  ]
+};
+
+
 app.use(express.json());
 
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  const name = req.query.name;
+  const job = req.query.job;
+  if (name != undefined && job == undefined){
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.status(200).send(result);
+  } else if (name != undefined || job != undefined) {
+    let result = findUserByNameAndJob(name, job);
+    result = { users_list: result };
+    res.status(200).send(result);
+  } else {
+    res.status(200).send(users);
+  }
 });
+
 
 app.listen(port, () => {
   console.log(
--- packages/react-frontend/src/Nav.jsx
+++ packages/react-frontend/src/Nav.jsx
@@ -5,13 +5,14 @@ function Nav() {
     <nav class="navbar navbar-inverse navbar-fixed-top fixed-top">
         <div class="container-fluid">
             <div class="navbar-header">
-                <a href="">Bite & Byte</a>
+                <a class="navbar-brand" href="#">Bite & Byte</a>
             </div>
             <div class="navbar-center">
                 <ul class="nav navbar-nav">
-                    <li><a href="">Page 1</a></li>
-                    <li><a href="">Page 2</a></li>
-                    <li><a href="">Page 3</a></li>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="page1.html">Page 1</a></li>
+                    <li><a href="#">Page 2</a></li>
+                    <li><a href="#">Page 3</a></li>
                 </ul>
             </div>
         </div>
