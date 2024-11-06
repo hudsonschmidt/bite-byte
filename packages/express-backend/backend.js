34e2bc780a5aae79098ff41344df31aa976a2532
Almas <apervmm@gmail.com>
2024-11-06 01:05:29 -0800

    recipes

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,63 +1,56 @@
 // backend.js
 import express from "express";
+import axios from "axios"; 
 
 const app = express();
 const port = 8000;
 
 
-const api = "07a042f2cdf746179eb011e0cafcfb4c"
-
-
-
-
-
-const recipes = {
-  recipes: [
-    {
-      id: "1",
-      meal: "Pasta Carbonara",
-      author: "Almas",
-    },
-    {
-      id: "2",
-      meal: "Mac&Cheese",
-      aruthor: "Max"
-    },
-    {
-      id: "3",
-      meal: "Pepperoni Pizza",
-      author: "Andy"
-    },
-    {
-      id: "4",
-      meal: "Ckicken Noodle Soup",
-      author: "Hudson"
-    },
-    {
-      id: "5",
-      meal: "Tartar",
-      author: "Kyle"
-    },
-  ]
-};
-
-
+const API_KEY = '07a042f2cdf746179eb011e0cafcfb4c'; 
+const API = 'https://api.spoonacular.com/recipes/random';
 app.use(express.json());
 
 
-app.get("/", (req, res) => {
-  const name = req.query.name;
-  const job = req.query.job;
-  if (name != undefined && job == undefined){
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.status(200).send(result);
-  } else if (name != undefined || job != undefined) {
-    let result = findUserByNameAndJob(name, job);
-    result = { users_list: result };
-    res.status(200).send(result);
-  } else {
-    res.status(200).send(users);
+function mapRecipeToSchema(recipe) {
+  return {
+    id: recipe.id,
+    name: recipe.title,
+    image_url: recipe.image,
+    // summary: recipe.summary,
+    ingredients: recipe.extendedIngredients ? recipe.extendedIngredients.map(ing => ing.name) : [],
+    // instructions: recipe.instructions || "No instructions available"
+  };
+}
+
+
+async function fetchRandom() {
+  try {
+    const response = await axios.get(API, {
+      params: {
+        apiKey: API_KEY,
+        number: 5,  
+      }
+    });
+    if (response.data && response.data.recipes) {
+      return response.data.recipes.map(mapRecipeToSchema);
+    } else {
+      console.error("Error: ", response.data);
+      return [];  
+    }
+  } catch (error) {
+    console.error('Failed to fetch meals', error);
+    return [];  
+  }
+}
+
+app.get('/recipes', async (req, res) => {
+  try {
+      let recipes = await fetchRandom();  
+      recipes = {recipes_list: recipes }
+      res.status(200).send(recipes);
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Failed to fetch' });
   }
 });
 
