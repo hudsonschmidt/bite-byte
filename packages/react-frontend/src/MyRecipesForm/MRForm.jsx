dda4616e18433e270491f16334be8565bd5ab766
hudsonschmidt <hudsonschmidt08@gmail.com>
2024-11-17 16:46:14 -0800

    created MyRecipe form

---
+++ packages/react-frontend/src/MyRecipesForm/MRForm.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./mrform.css"
+
+function MRForm() {
+    return (
+      <div class="container container-fluid" id='formbody'>
+          <h1>form</h1>
+      </div>
+    );
+  }
+  
+  export default MRForm;
