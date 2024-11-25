55daac4a16b7a08410f3ca42e22051009d4d4b9a
Almas <apervmm@gmail.com>
2024-11-24 20:54:39 -0800

    some changes

--- packages/react-frontend/src/Browse/Browse.jsx
+++ packages/react-frontend/src/Browse/Browse.jsx
@@ -1,61 +1,84 @@
 import React, { useState, useEffect } from 'react';
+import Form from './Form';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "./browse.css";
+import "./browse.css"
 
 const Browse = () => {
-    const [meals, setMeals] = useState([]);
-    const host = 'http://localhost:8000';
-    //const host = 'https://biteandbyte-cfd6d9azd2a4brce.westus-01.azurewebsites.net'
+  const [meals, setMeals] = useState([]);
+  //const host = 'https://biteandbyte-cfd6d9azd2a4brce.westus-01.azurewebsites.net'
+  const host = 'http://localhost:8000'
 
-    const fetchMeals = async () => {
-        try {
-            const response = await fetch(`${host}/meals`);
-            if (response.ok) {
-                const data = await response.json();
-                setMeals((prevMeals) => [...prevMeals, ...data.recipes_list]);
-            } else {
-                console.error('Failed to fetch meals:', response.statusText);
-            }
-        } catch (error) {
-            console.error('Failed to fetch meals:', error);
-        }
-    };
+  const fetchMeals = async () => {
+    try {
+      const response = await fetch(`${host}/meals`);
+      if (response.ok) {
+        const data = await response.json();
+        setMeals(data.recipes_list);
+      } else {
+        console.error('Failed to fetch meals:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Failed to fetch meals:', error);
+    }
+  };
 
+  const addMeal = async (meal) => {
+    try {
+      const response = await fetch(`${host}/meals`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NzM1MjNmMTVmMGMwYzk0MDk4MTdiOWIiLCJpYXQiOjE3MzI1MDk3NTMsImV4cCI6MTczMjU5NjE1M30.xSrjqvn2E2XL9lZnXIUbFoKmQFmodvw_o3eICO9o4h8',
+        },
+        body: JSON.stringify(meal),
+      });
 
-    useEffect(() => {
-        fetchMeals();
-    }, []);
+      if (response.ok) {
+        const newMeal = await response.json();
+        setMeals((prevMeals) => [newMeal, ...prevMeals]);
+      } else {
+        console.error('Failed to add meal:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Failed to add meal:', error);
+    }
+  };
 
-    const handleScroll = (e) => {
-        const bottom = Math.ceil(e.target.scrollTop + e.target.clientHeight) >= e.target.scrollHeight;
-        if (bottom) fetchMeals();
-    };
+  useEffect(() => {
+    fetchMeals();
+  }, []);
 
-    return (
-        <div id="browsebody" className="container-fluid">
-            <div className="row">
-                <div id="recipe-container"  className="col-md-9" style={{ overflowY: 'auto', height: 'calc(100vh - 56px)', padding: '20px' }} onScroll={handleScroll}>
-                    <div id="recipe-cards" className="row">
-                        {meals.map(meal => 
-                        (
-                          <div key={meal.id} className="col-md-4 mb-4">
-                              <div className="card">
-                                  <img 
-                                      src={meal.image_url || "/images/image.png"} 
-                                      className="card-img-top" 
-                                      alt={meal.name || "Recipe"} 
-                                  />
-                                  <div className="card-body">
-                                      <h5 className="card-title">{meal.name}</h5>
-                                  </div>
-                              </div>
-                          </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
+  const handleScroll = (e) => {
+    const bottom = Math.ceil(e.target.scrollTop + e.target.clientHeight) >= e.target.scrollHeight;
+    if (bottom) fetchMeals();
+  };
+
+  return (
+    <div id="browsebody" className="container-fluid">
+      <div className="row">
+        <div className="col-md-3 bg-light p-4">
+          <h2>Add a Reciepe</h2>
+          <Form handleSubmit={addMeal} />
         </div>
-    );
+
+        {/* Recipe Cards */}
+        <div id="recipe-container" className="col-md-9" style={{ overflowY: 'auto', height: 'calc(100vh - 56px)', padding: '20px' }} onScroll={handleScroll}>
+          <div id="recipe-cards" className="row">
+            {meals.map(meal => (
+              <div key={meal.id} className="col-md-4 mb-4">
+                <div className="card">
+                  <img src={meal.image_url} className="card-img-top" alt={meal.name} />
+                  <div className="card-body">
+                    <h5 className="card-title">{meal.name}</h5>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Browse;
